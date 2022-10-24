706b144f89fc43bdd03a26fba6ebcb05016e0fc7
Calladrus2001 <dugarvishesh@gmail.com>
2022-10-24 12:33:34 +0530

    feat: Logout

--- lib/Utils/constants.dart
+++ lib/Utils/constants.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 Color clr1 = Color(0xff6C96D2);
 String baseUrl =
-    "https://1f50-2401-4900-1c1a-70b-1413-b49b-e0a5-63cf.in.ngrok.io";
+    "https://b32e-2401-4900-1c1a-6cd3-3501-5d3a-57ba-cbf3.in.ngrok.io";
--- lib/Views/Profile.dart
+++ lib/Views/Profile.dart
@@ -1,4 +1,8 @@
+import 'package:code_star/Utils/constants.dart';
+import 'package:code_star/Views/Auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -8,14 +12,73 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  final box = GetStorage();
+  late String userID;
+
+  @override
+  void initState() {
+    userID = box.read("userID");
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        body: Column(
           children: [
-            SizedBox(height: 60),
+            Container(
+              height: 90,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: clr1,
+                  borderRadius:
+                      BorderRadius.only(bottomRight: Radius.circular(40))),
+              child: Column(
+                children: [
+                  SizedBox(height: 40),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Colors.white,
+                          child: Icon(
+                            Icons.account_circle_rounded,
+                            color: clr1,
+                          ),
+                        ),
+                        SizedBox(width: 16),
+                        Text("UserID: ${userID}",
+                            style: TextStyle(color: Colors.white))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(child: SizedBox(height: 1)),
+            GestureDetector(
+              child: Center(
+                  child: Chip(
+                elevation: 2.0,
+                backgroundColor: clr1,
+                label: Text("Logout",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.w500)),
+              )),
+              onTap: () {
+                setState(() {
+                  box.remove("userID");
+                  userID = "";
+                });
+                Get.to(() => AuthScreen());
+              },
+            ),
+            SizedBox(height: 20)
           ],
         ),
       ),

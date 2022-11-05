0f993362c89bc1803707d09847735790eb30bc12
Calladrus2001 <dugarvishesh@gmail.com>
2022-11-05 16:09:28 +0530

    feat: Reading test pt2

--- lib/Utils/constants.dart
+++ lib/Utils/constants.dart
@@ -4,4 +4,4 @@ Color clr1 = Color(0xff6C96D2);
 Map<int, String> difficult = {0: "Easy", 1: "Moderate", 2: "Hard", 3: "Custom"};
 
 String baseUrl =
-    "https://3ac6-2402-3a80-d6f-7610-4935-91f6-32a-5352.ap.ngrok.io";
+    "https://f5a6-2402-3a80-d6d-3ede-d512-8e34-60eb-fcfb.ap.ngrok.io";
